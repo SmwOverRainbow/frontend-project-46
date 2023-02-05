641aa7866443d1ebc5d209a933987361677b2fc8
mshcherbina <marinashcherbina89@gmail.com>
2023-02-05 23:05:26 +0300

    Add functions .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<type>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filePath1>')
+  .argument('<filePath2>')
+  .action((filePath1, filePath2) => {
+    genDiff(filePath1, filePath2);
+  });
 
 program.parse(process.argv);
 
 if (!program.args.length) program.help();
+
+// const opts = program.opts();
+
+// console.log(opts);
+// console.log(program.args);
