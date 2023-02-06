40e9273ce5073cbdf4d119c21f0d2c97c1387373
mshcherbina <marinashcherbina89@gmail.com>
2023-02-06 20:59:17 +0300

    Add tests for gendiff function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,14 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { expect, test } from '@jest/globals';
 import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import genDiff from '../src/index.js';
-import getPath from '../src/getterPath.js';
 
-const expectedValue = readFileSync(getPath('expectedPlaneFile.txt'), 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirName = dirname(__filename);
+
+const expectedValue = String(readFileSync(resolve(__dirName, '..', '__fixtures__', 'expectedPlainFile.txt'), 'utf-8'));
 
 test('get difference', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedValue);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedValue);
 });
