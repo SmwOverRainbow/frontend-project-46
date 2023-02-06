0c28791d143ee89a2bbb3b19f342e9c905f58e21
mshcherbina <marinashcherbina89@gmail.com>
2023-02-06 21:48:39 +0300

    Rename constants

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,10 @@ import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirName = dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirName = dirname(filename);
 
-const expectedValue = String(readFileSync(resolve(__dirName, '..', '__fixtures__', 'expectedPlainFile.txt'), 'utf-8'));
+const expectedValue = String(readFileSync(resolve(dirName, '..', '__fixtures__', 'expectedPlainFile.txt'), 'utf-8'));
 
 test('get difference', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedValue);
