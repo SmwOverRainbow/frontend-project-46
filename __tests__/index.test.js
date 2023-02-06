0546bb1d1718821d9683f39109caf32ff03011a2
mshcherbina <marinashcherbina89@gmail.com>
2023-02-06 14:58:48 +0300

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,8 @@ import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 import getPath from '../src/getterPath.js';
 
-const expectedValue = readFileSync(getPath('expectedPlaneFile.txt'));
+const expectedValue = readFileSync(getPath('expectedPlaneFile.txt'), 'utf-8');
 
 test('get difference', () => {
-  expect(genDiff()).toEqual(expectedValue);
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedValue);
 });
