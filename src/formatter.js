89dedb32c7e907d908fa9abb699a3eb59f7d521e
mshcherbina <marinashcherbina89@gmail.com>
2023-02-10 01:51:46 +0300

    Change logic for nested files

--- src/formatter.js
+++ src/formatter.js
@@ -1,45 +1,59 @@
 // import _ from 'lodash';
 
-const stringify = (value, indent = '    ', depth = 1) => {
-  if (typeof value !== 'object') {
-    return `${value}`;
-  }
-  const objToArr = Object.entries(value);
-  const elements = objToArr.map(([key, valueEl]) => {
-    let newValue = valueEl;
-    if (typeof valueEl === 'object' && valueEl !== null) {
-      newValue = stringify(valueEl, indent, depth + 1);
+const getCurrentIndent = (depth, intend = 4) => ' '.repeat(intend * depth - 2);
+const getClosingIndent = (depth, intend = 4) => ' '.repeat(intend * depth - intend);
+
+const stringify = (value, depth = 1) => {
+  const iter = (node, depthIter) => {
+    const iterIndent = getCurrentIndent(depthIter);
+    if (typeof value !== 'object' || value === null) {
+      return `${value}`;
     }
-    return `${indent.repeat(depth)}${key}: ${newValue}\n`;
-  });
-
-  const jsonFormat = elements.join('');
-  const result = `{\n${jsonFormat}${indent.repeat(depth - 1)}}`;
-  return result;
-};
-
-const getStyleFormat = (diffTree, style = 'stylish') => {
-  if (style === 'stylish') {
-    // const iter = (diffObjects, depth = 1, countOfRepeat = 1) => {
-    // const indent = '    ';
-    const result = diffTree.map((node) => {
-      switch (node.status) {
-        case 'nested':
-          return `    ${node.key}: ${getStyleFormat(node.children)}`;
-        case 'deleted':
-          return `  - ${node.key}: ${stringify(node.value)}`;
-        case 'added':
-          return `  + ${node.key}: ${stringify(node.value)}`;
-        case 'changed':
-          return `  - ${node.key}: ${stringify(node.value1)}\n  + ${node.key}: ${stringify(node.value2)}`;
-        case 'unchanged':
-          return `    ${node.key}: ${stringify(node.value)}`;
-        default: throw new Error('oops!');
+    const objToArr = Object.entries(node);
+    const elements = objToArr.map(([key, valueEl]) => {
+      let newValue = valueEl;
+      if (typeof valueEl === 'object') {
+        newValue = iter(valueEl, depthIter + 1);
       }
+      return `${iterIndent}  ${key}: ${newValue}`;
     });
 
-    console.log(result);
-    return `{\n${result.join('\n')}\n}`;
+    const closingIndent = getClosingIndent(depthIter);
+    const jsonFormat = elements.join('\n');
+    const result = `{\n${jsonFormat}\n${closingIndent}}`;
+    return result;
+  };
+  return iter(value, depth);
+};
+
+const getStyleFormat = (diffTree, style) => {
+  if (style === 'stylish') {
+    const iter = (diffObjects, depth = 1) => {
+      const iterIndent = getCurrentIndent(depth);
+      const closingIndent = getClosingIndent(depth);
+
+      const result = diffObjects.flatMap((node) => {
+        switch (node.status) {
+          case 'nested':
+            return `${iterIndent}  ${node.key}: ${iter(node.children, depth + 1)}`;
+          case 'deleted':
+            return `${iterIndent}- ${node.key}: ${stringify(node.value, depth + 1)}`;
+          case 'added':
+            return `${iterIndent}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
+          case 'changed':
+            return [`${iterIndent}- ${node.key}: ${stringify(node.value1, depth + 1)}`,
+              `${iterIndent}+ ${node.key}: ${stringify(node.value2, depth + 1)}`];
+          case 'unchanged':
+            return `${iterIndent}  ${node.key}: ${stringify(node.value, depth + 1)}`;
+          default: throw new Error('oops!');
+        }
+      });
+      return `{\n${result.join('\n')}\n${closingIndent}}`;
+    };
+
+    const stylishTree = iter(diffTree);
+    // console.log(stylishTree);
+    return stylishTree;
   }
   return 'another format';
 };
--- src/getDiff.js
+++ src/getDiff.js
@@ -5,29 +5,23 @@ const getDiffTree = (obj1, obj2) => {
   const sortedKeys = _.sortBy(keys);
 
   const diffTree = sortedKeys.map((key) => {
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { status: 'nested', key: [key], children: getDiffTree(obj1[key], obj2[key]) };
-    }
-    if (_.isObject(obj1[key]) && !obj2[key]) {
-      return { status: 'deleted', key: [key], value: _.cloneDeep(obj1[key]) };
-    }
-    if (_.isObject(obj2[key]) && !obj1[key]) {
-      return { status: 'added', key: [key], value: _.cloneDeep(obj2[key]) };
-    }
-    if (!_.has(obj1, key)) {
-      return { status: 'added', key: [key], value: obj2[key] };
-    }
-    if (!_.has(obj2, key)) {
-      return { status: 'deleted', key: [key], value: obj1[key] };
-    }
-    if (obj1[key] !== obj2[key]) {
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+        return { status: 'nested', key: [key], children: getDiffTree(obj1[key], obj2[key]) };
+      }
+      if (obj1[key] === obj2[key]) {
+        return { status: 'unchanged', key: [key], value: obj1[key] };
+      }
       return {
-        status: 'changed', key: [key], value1: obj1[key], value2: obj1[key],
+        status: 'changed', key: [key], value1: obj1[key], value2: obj2[key],
       };
     }
-    return { status: 'unchanged', key: [key], value: obj1[key] };
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      return { status: 'deleted', key: [key], value: obj1[key] };
+    }
+    return { status: 'added', key: [key], value: obj2[key] };
   });
-  console.log(diffTree);
+  // console.log(diffTree);
   return diffTree;
 };
 
