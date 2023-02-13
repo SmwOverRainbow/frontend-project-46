7c031ba65f1fd31720ded9dd2bce09b33f1cf9c7
mshcherbina <marinashcherbina89@gmail.com>
2023-02-13 14:58:33 +0300

    Rename function and constants

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ import getParse from './parser.js';
 import getFormat from './formatter/index.js';
 import getDiffTree from './getDiff.js';
 
-const getTypeFile = (pathOfFile) => {
+const getInputFormat = (pathOfFile) => {
   const components = pathOfFile.split('.');
-  const typeofFile = components.at(-1);
-  return typeofFile.toUpperCase();
+  const dataFormat = components.at(-1);
+  return dataFormat.toUpperCase();
 };
 
 const getFileContent = (pathOfFile) => readFileSync(resolve(cwd(), pathOfFile), 'utf-8');
 
 const genDiff = (filePath1, filePath2, format = 'stylish') => {
-  const fileType1 = getTypeFile(filePath1);
-  const fileType2 = getTypeFile(filePath2);
+  const dataFormat1 = getInputFormat(filePath1);
+  const dataFormat2 = getInputFormat(filePath2);
   const fileContent1 = getFileContent(filePath1);
   const fileContent2 = getFileContent(filePath2);
 
-  const obj1 = getParse(fileContent1, fileType1);
-  const obj2 = getParse(fileContent2, fileType2);
+  const obj1 = getParse(fileContent1, dataFormat1);
+  const obj2 = getParse(fileContent2, dataFormat2);
 
   const diffTree = getDiffTree(obj1, obj2);
   return getFormat(diffTree, format);
