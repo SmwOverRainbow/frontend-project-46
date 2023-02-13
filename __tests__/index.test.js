c4d3656b62d66efaabf5535385dcaa5a2ba43e9a
mshcherbina <marinashcherbina89@gmail.com>
2023-02-13 13:36:00 +0300

    Change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,34 +12,19 @@ const expectedValueStylish = readFile('expectedStylishDiff.txt');
 const expectedValuePlain = readFile('expectedPlainDiff.txt');
 const expectedValueJson = readFile('expectedJsonDiff.txt');
 
-test('diff JSON files with stylish format', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish')).toEqual(expectedValueStylish);
-});
-test('diff YAML files with stylish format', () => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish')).toEqual(expectedValueStylish);
-});
-
-test('diff JSON files with plain format', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(expectedValuePlain);
-});
-test('diff YAML files with plain format', () => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'plain')).toEqual(expectedValuePlain);
-});
-
-test('diff JSON files with json format', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(expectedValueJson);
-});
-test('diff YAML files with json format', () => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'json')).toEqual(expectedValueJson);
-});
-
 const cases = [
   ['__fixtures__/file1.json', '__fixtures__/file2.json', expectedValueStylish],
   ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', expectedValueStylish],
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', expectedValueStylish, 'stylish'],
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', expectedValueStylish, 'stylish'],
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', expectedValuePlain, 'plain'],
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', expectedValuePlain, 'plain'],
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', expectedValueJson, 'json'],
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', expectedValueJson, 'json'],
 ];
 
-describe('default format', () => {
-  test.each(cases)('difference %s and %s', (a, b, expectedResult) => {
-    expect(genDiff(a, b)).toEqual(expectedResult);
+describe('output format', () => {
+  test.each(cases)('difference %s and %s', (a, b, expectedResult, format = 'stylish') => {
+    expect(genDiff(a, b, format)).toEqual(expectedResult);
   });
 });
